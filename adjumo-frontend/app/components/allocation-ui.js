ef9352c8146058b4aaf766481513a01e97dd4337
Philip Belesky <contact@philipbelesky.com>
2015-12-10 17:58:14 +1300

    Removing dragula elements
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- adjumo-frontend/app/components/allocation-ui.js
+++ adjumo-frontend/app/components/allocation-ui.js
@@ -2,46 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  adj_dragulaconfig: {
-    accepts: function (el, target, source, sibling) {
-      return true; // elements can be dropped in any of the `containers` by default
-    },
-    options: {
-        copy: false,
-        revertOnSpill: false,
-        removeOnSpill: false,
-        direction: 'horizontal',
-        // Other options from the dragula source page.
-    },
-    enabledEvents: ['drag', 'drop']
-  },
-
   actions: {
 
     toggleGender: function() {
       console.log("Toggling adj");
-    },
-
-    adjDrag: function(obj) {
-      // console.log('starting adj drag');
-      // console.log(obj);
-      // console.log(ops);
-    },
-
-    adjDrop: function(obj) {
-      console.log('did an adj drop');
-
-      var adj = this.$(obj[0]);
-
-      console.log(adj);
-      console.log(adj.get('model'));
-      console.log(adj.get('sefa'));
-      console.log(adj.get('context')); // the actual div
-
-      var toPosition = obj[1];
-      //console.log(toPosition);
-      var fromPosition = obj[2];
-      //console.log(fromPosition);
     },
 
   }
