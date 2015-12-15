7d8abde4829bcaa7a92b8c51ad6275975ccc2581
Chuan-Zheng Lee <czlee@stanford.edu>
2015-12-15 11:55:36 -0800

    Write all results of trial.jl to JSON files

--- julia/trial.jl
+++ julia/trial.jl
@@ -1,6 +1,7 @@
 push!(LOAD_PATH, Base.source_dir())
 using ArgParse
 using Adjumo
+using JsonAPI
 
 include("random.jl")
 
@@ -21,6 +22,9 @@ argsettings = ArgParseSettings()
     "--enforce-team-conflicts"
         help = "Enforce team-adjudicator conflicts (as opposed to just penalize)"
         action = :store_true
+    "--json-dir"
+        help = "Where to write JSON files upon completion."
+        default = "../adjumo-frontend/public/data"
 end
 args = parse_args(ARGS, argsettings)
 
@@ -40,6 +44,49 @@ componentweights.adjconflict = 1e6
 roundinfo.componentweights = componentweights
 
 allocations = allocateadjudicators(roundinfo; solver=args["solver"], enforceteamconflicts=args["enforce-team-conflicts"])
+
+println("Writing JSON files...")
+directory = args["json-dir"]
+standard_fields = [
+    :adjudicators,
+    :teams,
+    :institutions,
+    :debates,
+    :adjadjconflicts,
+    :teamadjconflicts,
+    :lockedadjs,
+    :blockedadjs,
+    :componentweights,
+]
+
+for field in standard_fields
+    filename = joinpath(directory, string(field)*".json")
+    println("Creating $filename")
+    f = open(filename, "w")
+    exportjson(f, roundinfo, field)
+    close(f)
+end
+
+special_fields = [
+    "adjadjhistory",
+    "teamadjhistory",
+    "groupedadjs"
+]
+
+for field in special_fields
+    filename = joinpath(directory, field*".json")
+    println("Creating $filename")
+    f = open(filename, "w")
+    func = eval(symbol("exportjson"*field))
+    func(f, roundinfo)
+    close(f)
+end
+
+filename = joinpath(directory, "allocations.json")
+println("Creating $filename")
+f = open(filename, "w")
+printjsonapi(f, allocations)
+close(f)
 
 showconstraints(roundinfo)
 for allocation in allocations
