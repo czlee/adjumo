639bc8b92cd5d781c0b19ee3b1b9a1fb843faf8f
Philip Belesky <contact@philipbelesky.com>
2015-12-23 22:37:37 +1100

    WIP on tabbie export
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- frontend/app/components/navbar/export-ui.js
+++ frontend/app/components/navbar/export-ui.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  store: Ember.inject.service()
+  store: Ember.inject.service(),
 
   actions: {
 
@@ -10,26 +10,42 @@ export default Ember.Component.extend({
 
       console.log('starting to create a new allocation for #' + event.target.value);
 
-      var debateData = [];
-      this.store.findAll('debate').then((debate) => {
+      // Find the selected allocation iteration
+      var selectedAllocation;
+      this.get('store').findAll('allocation-iteration').forEach(function(allocation) {
+        if (allocation.get('id') === allocationID) {
+          selectedAllocation = allocation;
+          console.log('set allocation');
+        }
+      });
 
-        // ASYNC: waiting for find
-        debate.forEach(function(debate) {
+      var exportData = [];
 
-          debateData.push({
-            id: debate.get('id'),
-            panel: {
-              "chair": <adjid>,
-              "panellists": [<adjid>, <adjid>, ...],
-              "trainees": [<adjid>, <adjid>, ...]
-            },
-            strength: 50, // This should be a range from 1-100, have no idea how to generate
-            messages: [null],
-          }
+      selectedAllocation.get('panels').forEach(function(panel) {
 
+        var panellistIDs = [];
+        panel.get('panellists').forEach(function(adj) {
+          panellistIDs.push(adj.get('id'));
         });
 
-      }
+        var traineeIDs = [];
+        panel.get('panellists').forEach(function(adj) {
+          panellistIDs.push(adj.get('id'));
+        });
+
+        exportData.push({
+          id: panel.get('debate').get('id'),
+          panel: {
+            "chair": panel.get('chair').get('id'),
+            "panellists": panellistIDs,
+            "trainees": traineeIDs,
+          },
+          strength: 50, // This should be a range from 1-100, have no idea how to generate
+          messages: [null],
+        });
+
+      });
+
 
     }
 
