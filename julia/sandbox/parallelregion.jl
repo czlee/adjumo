a5a65e3012323041f50fb21e80f5f702b6c17eee
Chuan-Zheng Lee <czlee@stanford.edu>
2015-12-24 17:42:21 +0100

    Update parallelregion.jl

--- julia/sandbox/parallelregion.jl
+++ julia/sandbox/parallelregion.jl
@@ -35,7 +35,8 @@ function regionalrepresentationmatrix_dist(feasiblepanels::Vector{AdjudicatorPan
 
     # Parallelize this part, it's heavy
     Πα = SharedArray(Float64, (ndebates, npanels))
-    @time @sync @parallel for (p, panel) in enumerate(feasiblepanels)
+    @time @sync @parallel for p in 1:length(feasiblepanels)
+        panel = feasiblepanels[p]
         for (d, dinfo) in enumerate(debateinfos)
             Πα[d,p] = panelregionalrepresentationscore(dinfo..., panel)
         end
