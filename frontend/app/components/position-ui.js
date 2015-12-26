cd8d2c2f920ec147edf197b6f27665ba08504663
Philip Belesky <contact@philipbelesky.com>
2015-12-26 16:52:18 +1100

    Fixed bug with chair swapping
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- frontend/app/components/position-ui.js
+++ frontend/app/components/position-ui.js
@@ -16,48 +16,50 @@ export default Ember.Component.extend(DroppableMixin, {
 
     if (positionType === 'chair') {
       // If coming from a chair
-      var currentAdj = this.get('panel').get('chair');
+      var hasCurrentAdj = this.get('panel').get('chair').get('content');
 
       if (droppedAdj === droppedAdjOldPanel.get('chair').get('content')) {
         // If the dropped adj was previously in the chairing position
-        if (currentAdj) {
-          droppedAdjOldPanel.set('chair', currentAdj); // Set the previous chair position as the adj dropped upon
-          currentAdj.set('panel', droppedAdjOldPanel);
+        if (hasCurrentAdj !== null) {
+          droppedAdjOldPanel.set('chair', this.get('panel').get('chair')); // Set the previous chair position as the adj dropped upon
+          this.get('panel').get('chair').set('panel', droppedAdjOldPanel);
+        } else {
+          droppedAdjOldPanel.set('chair', null); // Set the previous chair position as the adj dropped upon
         }
       } else if (droppedAdjOldPanel.get('panellists').contains(droppedAdj)) {
         // If the dropped adj was previously a panellist
         droppedAdjOldPanel.get('panellists').removeObject(droppedAdj); // Remove the dropped adj from the previous panel
 
-        if (currentAdj.get('content')) {
+        if (hasCurrentAdj !== null) {
           // If the chairing position dropped to is NOT currently occupied
           if (droppedAdjOldPanel.get('panellists').get('length') > 0) {
             // Stupid hack seemingly needed here to reconstruct an array of pannelists
             // For some reason we cant just oldPanellists.pushObject(currentAdj);
-            var newPanel = [currentAdj];
+            var newPanel = [this.get('panel').get('chair')];
             droppedAdjOldPanel.get('panellists').forEach(function(item) {
               newPanel.push(item);
             });
             droppedAdjOldPanel.set('panellists', newPanel);
           } else {
-            droppedAdjOldPanel.set('panellists', [currentAdj]); // If the previous panel is empty we can just move them directly
+            droppedAdjOldPanel.set('panellists', [this.get('panel').get('chair')]); // If the previous panel is empty we can just move them directly
           }
         }
       } else if (droppedAdjOldPanel.get('trainees').contains(droppedAdj)) {
         // If the dropped adj was previously a trainee
         droppedAdjOldPanel.get('trainees').removeObject(droppedAdj);
 
-        if (currentAdj.get('content')) {
+        if (hasCurrentAdj !== null) {
           // If the chairing position dropped to is NOT currently occupied
           if (droppedAdjOldPanel.get('trainees').get('length') > 0) {
             // Stupid hack seemingly needed here to reconstruct an array of pannelists
             // For some reason we cant just oldPanellists.pushObject(currentAdj);
-            var newPanel = [currentAdj];
+            var newPanel = [this.get('panel').get('chair')];
             droppedAdjOldPanel.get('trainees').forEach(function(item) {
               newPanel.push(item);
             });
             droppedAdjOldPanel.set('trainees', newPanel);
           } else {
-            droppedAdjOldPanel.set('trainees', [currentAdj]); // If the previous panel is empty we can just move them directly
+            droppedAdjOldPanel.set('trainees', [this.get('panel').get('chair')]); // If the previous panel is empty we can just move them directly
           }
         }
       }
