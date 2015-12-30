c1c7cffbdb27b712b4e81bc52ac68001a85f3e1a
Chuan-Zheng Lee <czlee@stanford.edu>
2015-12-31 00:45:29 +0200

    Typos

--- julia/Adjumo/src/debateweights.jl
+++ julia/Adjumo/src/debateweights.jl
@@ -4,13 +4,13 @@ OPEN_DEBATE_WEIGHT_MAPS = Dict{Int,Vector{Float64}}(
     9=>Float64[1, 2, 1, 2, 1, 2],
 )
 
-ESL_DEBATE_WEIGHT_MAP = Dict{Int,Vector{Float64}}(
+ESL_DEBATE_WEIGHT_MAPS = Dict{Int,Vector{Float64}}(
     7=>Float64[1, 1, 3.8, 4, 4.2, 4.4, 4.6, 4.8, 5, 5, 5, 5, 4.8, 4.6, 4.4, 4.2, 4, 3.8, 3.6, 3.4, 3, 3, 3, 3, 3],
     8=>Float64[1, 1, 1, 1, 1, 1],
     9=>Float64[1, 2, 1, 2, 1, 2],
 )
 
-EFL_DEBATE_WEIGHT_MAP = Dict{Int,Vector{Float64}}(
+EFL_DEBATE_WEIGHT_MAPS = Dict{Int,Vector{Float64}}(
     7=>Float64[1, 1, 3.8, 4, 4.2, 4.4, 4.6, 4.8, 5, 5, 5, 5, 4.8, 4.6, 4.4, 4.2, 4, 3.8, 3.6, 3.4, 3, 3, 3, 3, 3],
     8=>Float64[1, 1, 1, 1, 1, 1],
     9=>Float64[1, 2, 1, 2, 1, 2],
@@ -22,15 +22,15 @@ function computedebateweights!(roundinfo::RoundInfo)
     for debate in roundinfo.debates
         weight = 0.0
         for team in debate.teams
-            weight = OPEN_DEBATE_WEIGHT_MAP[roundinfo.currentround][team.points+1]
+            weight = OPEN_DEBATE_WEIGHT_MAPS[roundinfo.currentround][team.points+1]
             if team.language == EnglishSecond || team.language == EnglishForeign
-                eslweight = ESL_DEBATE_WEIGHT_MAP[roundinfo.currentround][team.points+1]
+                eslweight = ESL_DEBATE_WEIGHT_MAPS[roundinfo.currentround][team.points+1]
                 if eslweight > weight
                     weight = eslweight
                 end
             end
             if team.language == EnglishForeign
-                eflweight = EFL_DEBATE_WEIGHT_MAP[roundinfo.currentround][team.points+1]
+                eflweight = EFL_DEBATE_WEIGHT_MAPS[roundinfo.currentround][team.points+1]
                 if eflweight > weight
                     weight = eflweight
                 end
